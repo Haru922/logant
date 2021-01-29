81595621742f903cad4c93dcd5f3cb11dc7c012a
Haru <jbh922@gmail.com>
2021-01-29 12:23:49 +0900

    antcli -option flags added.

--- antcli.py
+++ antcli.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
+
 import configparser
 import argparse
 import datetime
 import sqlite3
 
-LOGANT_CONF = '/home/haru/logant.conf'
+LOGANT_CONF = '/usr/lib/gooroom-security-utils/logant.conf'
 
-def get_log(c, syslog_identifier, log_start_time, log_end_time, verbose):
+def get_log(c, syslog_identifier, log_start_time, log_end_time, details, verbose):
     if syslog_identifier:
         query = ''' SELECT *
                     FROM GOOROOM_SECURITY_LOG
@@ -20,18 +22,24 @@ def get_log(c, syslog_identifier, log_start_time, log_end_time, verbose):
                     ORDER BY __REALTIME_TIMESTAMP '''
         c.execute(query, (log_start_time, log_end_time,))
 
-    for d in c:
+    keys = ''
+    for row in c:
+        if not keys:
+            keys = row.keys() if details else ['__REALTIME_TIMESTAMP', 'PRIORITY', 'MESSAGE', 'GRMCODE', 'SYSLOG_IDENTIFIER']
         if verbose:
-            print(' '.join([str(d[i]) for i in d.keys()]))
+            for key in keys:
+                print('{}: {}'.format(key, row[key]))
+            print()
         else:
-            print(d['__REALTIME_TIMESTAMP'], d['PRIORITY'], d['MESSAGE'], d['GRMCODE'], d['SYSLOG_IDENTIFIER'])
-
+            print(' '.join([str(row[key]) for key in keys]))
+            
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--id', type=str, help='syslog identifier.', default='')
-    parser.add_argument('-s', '--start', type=str, help='log start time.', default='1')
-    parser.add_argument('-e', '--end', type=str, help='log end time.', default=str(datetime.datetime.now()))
-    parser.add_argument('-v', '--verbose', help='display all columns.', action='store_true')
+    parser.add_argument('-s', '--start', type=str, help="log start time. (FORMAT: 'Year-Month-Day Hour:Minutes:Seconds')", default='1')
+    parser.add_argument('-e', '--end', type=str, help="log end time. (FORMAT: 'Year-Month-Day Hour:Minutes:Seconds')", default=str(datetime.datetime.now()))
+    parser.add_argument('-d', '--details', help='display details.', action='store_true')
+    parser.add_argument('-v', '--verbose', help='display with column names.', action='store_true')
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
@@ -42,6 +50,6 @@ if __name__ == '__main__':
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    get_log(c, args.id, args.start, args.end, args.verbose)
+    get_log(c, args.id, args.start, args.end, args.details, args.verbose)
 
     conn.close()
